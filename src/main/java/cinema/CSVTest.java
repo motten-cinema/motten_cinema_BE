23ac2d93c1493694508c0483e18173f01d470016
Wonyoung Kim <wonyoung020626@gmail.com>
2025-05-25 21:53:22 +0900

    test : Add CSVTest.java and Edit ScheduleVO.java to use schedule.csv

---
+++ src/main/java/cinema/CSVTest.java
@@ -0,0 +1,19 @@
+package cinema;
+
+import cinema.domain.ScheduleVO;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileReader;
+import java.util.List;
+
+public class CSVTest {
+    public static void main(String[] args) throws Exception {
+        List<ScheduleVO> schedules = new CsvToBeanBuilder<ScheduleVO>(new FileReader("src/main/resources/schedule.csv"))
+                .withType(ScheduleVO.class)
+                .build()
+                .parse();
+
+        schedules.forEach(System.out::println);
+
+    }
+}
--- src/main/java/cinema/domain/ScheduleVO.java
+++ src/main/java/cinema/domain/ScheduleVO.java
@@ -1,5 +1,6 @@
 package cinema.domain;
 
+import com.opencsv.bean.CsvDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,15 @@ import java.time.LocalTime;
 public class ScheduleVO {
     private int schedule_id;
     private int movie_id;
+
+    // OpenCSV는 String을 LocalDate로 자동 변환할 수 없다.
+    // @CsvDate를 통해 날짜 형식을 명시하면 자동으로 LocalDate와 LocalTime으로 변환할 수 있다.
+    @CsvDate("yyyy-MM-dd")
     private LocalDate screen_date;
+
+    @CsvDate("HH:mm:ss")
     private LocalTime start_time;
+
+    @CsvDate("HH:mm:ss")
     private LocalTime end_time;
 }
