f55d2bf744ef8c6a9ab5ed51a418fb16a9763a99
YeonWoo choi <rryunn@naver.com>
2025-05-25 16:06:18 +0900

    feat: cinema dao 구현

--- src/main/java/cinema/dao/CinemaDaoImpl.java
+++ src/main/java/cinema/dao/CinemaDaoImpl.java
@@ -1,22 +1,17 @@
 package cinema.dao;
+
 import cinema.domain.MovieVO;
 import database.JDBCUtil;
-import java.sql.Connection;
-import java.sql.ResultSet;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
 import java.util.Optional;
 
 public class CinemaDaoImpl implements CinemaDao {
-    Connection conn = JDBCUtil.getConnection();
 
-    private final String MOVIE_LIST = "select * from movie";
-    private final String MOVIE_GET = "select * from movie where movie_id=?";
-    private final String MOVIE_SORT_BY_RATING_DESC = "select * from movie order by rating desc";
-    private final String MOVIE_SORT_BY_RATING_ASC = "select * from movie order by rating asc";
-    private final String MOVIE_BY_GENRE = "select * from movie where genre=?";
+    private final Connection conn = JDBCUtil.getConnection();
+
     @Override
     public void insert(MovieVO movie) throws SQLException {
         String sql = "INSERT INTO movie (movie_id, title, genre, rating, director, cast, summary, age_limit, duration, release_date) " +
@@ -32,46 +27,31 @@ public class CinemaDaoImpl implements CinemaDao {
             ps.setString(7, movie.getSummary());
             ps.setInt(8, movie.getAge_limit());
             ps.setInt(9, movie.getDuration());
-            ps.setDate(10, java.sql.Date.valueOf(movie.getRelease_date()));
+            ps.setDate(10, Date.valueOf(movie.getRelease_date()));
             ps.executeUpdate();
         }
     }
 
     @Override
-    public List<MovieVO> getList() throws SQLException{
-        List<MovieVO> movieList = new ArrayList<MovieVO>();
-        try(PreparedStatement ps = conn.prepareStatement(MOVIE_LIST);
-            ResultSet rs = ps.executeQuery();){
-            while(rs.next()){
-                MovieVO movie = map(rs);
-                movieList.add(movie);
+    public List<MovieVO> getList() throws SQLException {
+        String sql = "SELECT * FROM movie";
+        List<MovieVO> movieList = new ArrayList<>();
+        try (PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                movieList.add(map(rs));
             }
         }
         return movieList;
     }
 
-    private MovieVO map(ResultSet rs) throws SQLException {
-        MovieVO movie = new MovieVO();
-        movie.setMovie_id(rs.getInt("MOVIE_ID"));
-        movie.setTitle(rs.getString("TITLE"));
-        movie.setGenre(rs.getString("GENRE"));
-        movie.setRating(rs.getFloat("RATING"));
-        movie.setDirector(rs.getString("DIRECTOR"));
-        movie.setCast(rs.getString("CAST"));
-        movie.setSummary(rs.getString("SUMMARY"));
-        movie.setAge_limit(rs.getInt("AGE_LIMIT"));
-        movie.setDuration(rs.getInt("DURATION"));
-        movie.setRelease_date(rs.getDate("RELEASE_DATE").toLocalDate());
-        return movie;
-    }
-
-
     @Override
-    public Optional<MovieVO> get(String id) throws SQLException{
-        try(PreparedStatement ps = conn.prepareStatement(MOVIE_GET)){
+    public Optional<MovieVO> get(String id) throws SQLException {
+        String sql = "SELECT * FROM movie WHERE movie_id = ?";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, id);
-            try(ResultSet rs = ps.executeQuery()){
-                if(rs.next()){
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
                     return Optional.of(map(rs));
                 }
             }
@@ -80,29 +60,47 @@ public class CinemaDaoImpl implements CinemaDao {
     }
 
     @Override
-    public List<MovieVO> getSortedByRating(boolean descending) throws SQLException{
-        String sql = descending ? MOVIE_SORT_BY_RATING_DESC : MOVIE_SORT_BY_RATING_ASC;
+    public List<MovieVO> getSortedByRating(boolean descending) throws SQLException {
+        String sql = descending
+                ? "SELECT * FROM movie ORDER BY rating DESC"
+                : "SELECT * FROM movie ORDER BY rating ASC";
         List<MovieVO> list = new ArrayList<>();
-        try(PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();){
-            while(rs.next()) {
+        try (PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
                 list.add(map(rs));
             }
         }
         return list;
     }
+
     @Override
     public List<MovieVO> getByGenre(String genre) throws SQLException {
+        String sql = "SELECT * FROM movie WHERE genre = ?";
         List<MovieVO> list = new ArrayList<>();
-        try(PreparedStatement ps = conn.prepareStatement(MOVIE_BY_GENRE)) {
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, genre);
-            try(ResultSet rs = ps.executeQuery()) {
-                while(rs.next()) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
                     list.add(map(rs));
                 }
             }
         }
         return list;
     }
-}
 
+    private MovieVO map(ResultSet rs) throws SQLException {
+        MovieVO movie = new MovieVO();
+        movie.setMovie_id(rs.getInt("movie_id"));
+        movie.setTitle(rs.getString("title"));
+        movie.setGenre(rs.getString("genre"));
+        movie.setRating(rs.getFloat("rating"));
+        movie.setDirector(rs.getString("director"));
+        movie.setCast(rs.getString("cast"));
+        movie.setSummary(rs.getString("summary"));
+        movie.setAge_limit(rs.getInt("age_limit"));
+        movie.setDuration(rs.getInt("duration"));
+        movie.setRelease_date(rs.getDate("release_date").toLocalDate());
+        return movie;
+    }
+}
