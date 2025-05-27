ce5a6ef75a75ef4f55ff8e001b1a08b2fc611651
kimhana11 <dd1246gksk@gmail.com>
2025-05-27 16:24:33 +0900

    Feat: 좌석id 조회하는 dao 추가

--- src/main/java/seat/dao/ReservationSeatDao.java
+++ src/main/java/seat/dao/ReservationSeatDao.java
@@ -6,8 +6,7 @@ import java.util.List;
 
 public interface ReservationSeatDao {
     void insert(ReservationSeatVO reservationSeat); //예매-좌석 매핑 정보를 저장
-
+    List<Integer> findSeatIdsByReservationId(String reservationId); //예매 id로 좌석 id 조회
     void deleteByReservationId(String reservationId);// 예매 ID에 해당하는 매핑 정보 삭제
-
     List<String> findSeatCodesByReservationId(String reservationId);  // 예매 ID로 좌석 코드(seat_code) 목록 조회
 }
