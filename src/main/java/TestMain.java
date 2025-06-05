161c7f2921889c95bd64ebfaf8c2ee4a0a5ddffc
YeonWoo choi <rryunn@naver.com>
2025-06-05 17:56:27 +0900

    fix: testMain conflict 해결

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,6 +1,5 @@
 import command.Command;
 import command.ReserveTicketCommand;
-import command.print.MovieViewImpl;
 import reservation.service.ReservationService;
 import reservation.service.ReservationServiceImpl;
 import schedule.service.ScheduleService;
@@ -8,12 +7,8 @@ import schedule.service.ScheduleServiceImpl;
 import seat.service.SeatService;
 import seat.service.SeatServiceImpl;
 
-import java.util.Scanner;
-
 public class TestMain {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        MovieViewImpl.printMovieList(sc);
         ScheduleService scheduleService = new ScheduleServiceImpl();
         SeatService seatService = new SeatServiceImpl();
         ReservationService reservationService = new ReservationServiceImpl();
