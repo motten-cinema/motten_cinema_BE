f6bd168cf9aeb8c43b580c6ff16256f82560baad
YeonWoo choi <rryunn@naver.com>
2025-06-05 17:59:10 +0900

    feat: 상영정보 조회 테스트 추가

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,5 +1,6 @@
 import command.Command;
 import command.ReserveTicketCommand;
+import command.print.MovieViewImpl;
 import reservation.service.ReservationService;
 import reservation.service.ReservationServiceImpl;
 import schedule.service.ScheduleService;
@@ -7,8 +8,12 @@ import schedule.service.ScheduleServiceImpl;
 import seat.service.SeatService;
 import seat.service.SeatServiceImpl;
 
+import java.util.Scanner;
+
 public class TestMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MovieViewImpl.printMovieList(sc);
         ScheduleService scheduleService = new ScheduleServiceImpl();
         SeatService seatService = new SeatServiceImpl();
         ReservationService reservationService = new ReservationServiceImpl();
